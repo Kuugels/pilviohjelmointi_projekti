57f3861220ec034818dc2d456f74641f7e6b3602
Smuli <samuli.kakonen@metropolia.fi>
2018-10-08 23:05:26 +0300

    Heroku db-connection

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('foorumi', '', '', {
-  dialect: 'sqlite',
-  storage: 'db/database.sqlite'
+var sequelize = new Sequelize('postgres://gviikmmkzqqevt:29dba107e9662470caf935d27a31398fc2351c9897a421228e3b2fdc10e8369e@ec2-54-217-236-201.eu-west-1.compute.amazonaws.com:5432/d1eq1pe64fplpo', {
+  dialect: 'postgres',
+  storage: 'postgres'
 });
 
 module.exports = {
