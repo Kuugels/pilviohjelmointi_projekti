2aefa0054a6505ddf903d5be2c758a9cae390c6d
Smuli <samuli.kakonen@metropolia.fi>
2018-10-08 23:58:43 +0300

    postgres fix v3

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,7 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('postgres://spwxemvyyyaoek:fe1bf2fd87f2dd778c0d690bffafd698a7622dabaf15f16788deac742c382b89@ec2-54-217-235-166.eu-west-1.compute.amazonaws.com:5432/d4upbqs1n61q51', {
+var sequelize = new Sequelize('ec2-54-217-235-166.eu-west-1.compute.amazonaws.com/d4upbqs1n61q51', 'spwxemvyyyaoek', 'fe1bf2fd87f2dd778c0d690bffafd698a7622dabaf15f16788deac742c382b89', {
  dialect: 'postgres',
- protocol: 'postgres'
 });
 
 //postgres://spwxemvyyyaoek:fe1bf2fd87f2dd778c0d690bffafd698a7622dabaf15f16788deac742c382b89@ec2-54-217-235-166.eu-west-1.compute.amazonaws.com:5432/d4upbqs1n61q51
