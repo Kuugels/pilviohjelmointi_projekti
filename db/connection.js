72aafabe3999661b56ef3009c28ab2e13ebb64f4
Smuli <samuli.kakonen@metropolia.fi>
2018-10-08 23:31:27 +0300

    postgres fix v2

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('ec2-54-217-236-201.eu-west-1.compute.amazonaws.com:5432/d1eq1pe64fplpo', 'gviikmmkzqqevt', '29dba107e9662470caf935d27a31398fc2351c9897a421228e3b2fdc10e8369e', {
-  dialect: 'postgres'
+var sequelize = new Sequelize('postgres://spwxemvyyyaoek:fe1bf2fd87f2dd778c0d690bffafd698a7622dabaf15f16788deac742c382b89@ec2-54-217-235-166.eu-west-1.compute.amazonaws.com:5432/d4upbqs1n61q51', {
+ dialect: 'postgres',
+ protocol: 'postgres'
 });
-//,
-//storage: 'postgres'
+
+//postgres://spwxemvyyyaoek:fe1bf2fd87f2dd778c0d690bffafd698a7622dabaf15f16788deac742c382b89@ec2-54-217-235-166.eu-west-1.compute.amazonaws.com:5432/d4upbqs1n61q51
+
 module.exports = {
   DataTypes: Sequelize,
   sequelize: sequelize
